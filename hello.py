2b0e4553c6298396f245c98b568ca819319cf32a
Sahil Bajaj <sahil29@gmail.com>
2013-09-30 22:29:40 +0800

    Make BSON to JSON serialization work
    * Added routes that return lists of KGs and sellers (temporary)

--- hello.py
+++ hello.py
@@ -1,35 +1,72 @@
 import os
 import datetime
 import pymongo
-from flask import Flask
+from flask import Flask, json, jsonify
 from pymongo import MongoClient
+from bson.json_util import dumps
+
+API_VERSION = '1.0'
+API_ROUTE_PREFIX = '/api/' + API_VERSION + '/'
+MONGO_URL = os.environ.get('MONGOHQ_URL')
 
 app = Flask(__name__)
-# app.config['DEBUG'] = True
+app.config['DEBUG'] = True
+
+# Set up database
+client = MongoClient(MONGO_URL)
+db = client.app18404502
+collection = db.kgdata
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+	return 'Nothing to do here.'
 
-@app.route('/something')
-def something():
-    return 'something!'
+# List all karung gunis
+@app.route(API_ROUTE_PREFIX + 'karunggunis')
+def karungGunis():
+	result = [dumps(document) for document in collection.find({ 'role': 'kg' })]
+	return jsonify(result=result)
 
+# List all sellers
+@app.route(API_ROUTE_PREFIX + 'sellers')
+def sellers():
+	result = [dumps(document) for document in collection.find({ 'role': 'seller' })]
+	return jsonify(result=result)
+
+# Test method
 @app.route('/mongo')
 def mongo():
-    # db = app18404502
-    MONGO_URL = os.environ.get('MONGOHQ_URL')
+	# Specify the database
+	# Print a list of collections
+	# print db.collection_names()
 
-    app.logger.debug(MONGO_URL)
+	# Specify the collection, in this case 'monsters'
 
-    #connection = Connection(MONGO_URL)
-    client = MongoClient(MONGO_URL)
+	# Get a count of the documents in this collection
+	count = collection.count()
+	# print "The number of documents you have in this collection is:", count
 
-    # Specify the database
-    db = client.app18404502
-    app.logger.debug(db.collection_names)
-    # msg = db.collection_names
-    app.logger.debug(db.collection_names()[0])
-    str = db.collection_names()[0]
-    # Print a list of collections
-    return str
+	# Create a document for a monster
+	monster = {"name": "Vampirus",
+			   "occupation": "Blood Sucker",
+			   "tags": ["vampire", "teeth", "bat"],
+			   "date": datetime.datetime.utcnow()
+			   }
+
+	# Insert the monster document into the monsters collection
+	# monster_id = collection.insert(monster)
+
+	# Print out our monster documents
+	# for monster in collection.find():
+		# print monster
+	# results = collection.find()
+	result = [dumps(document) for document in collection.find()]
+	app.logger.debug(collection.find()[0])
+	return jsonify(res=result)
+
+	# # Query for a particular monster
+	# print collection.find_one({"name": "Dracula"})
+
+# Run Flask application
+if __name__ == '__main__':
+	app.run()
